d6b15c2a5a930d307c818dad6495c8e50a1c59f3
Enoch Otieno <enochotieno@rocketmail.com>
2022-06-07 05:06:22 +0300

    Added the readme file to the project

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 import qrcode
+import os. path
+import time as time
+
+# initialize qr code library
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
     box_size=10,
     border=4,
 )
-url = print(input("Enter the link you'd like to generate:\n"))
-qr.add_data(url)
+print("=========================\n")
+# prompt user to key in the value they would like to serialize using qr  
+url = input("Enter the link you'd like to generate:\n")
+shop = input("Enter the name of the vendor/shop:\n")
+sanitize = "https://"+url
+print(sanitize)
+print("===========================\n")
+qr.add_data(sanitize)
 qr.make(fit=True)
-
-img = qr.make_image(fill_color="black", back_color="white")
+# You can change the color of the files
+img = qr.make_image(fill_color="#0a3d62", back_color="white")
 type(img)  # qrcode.image.pil.PilImage
-img.save(url.to_str()+".png")
+# Save the image
+img.save('images/'+shop+'.jpg')
+print("==========================\n")
+print("QR code generated\n")
+print("==========================")
