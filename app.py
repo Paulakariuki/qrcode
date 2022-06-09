cff76bec2209c429f03e2b509255d9dcee7bc7d1
Enoch Otieno <enochotieno@rocketmail.com>
2022-06-09 11:08:49 +0300

    Addition of a for loop to the code to allow the user to enter how many times they'd like to iterate over the process

--- app.py
+++ app.py
@@ -2,27 +2,32 @@ import qrcode
 import os. path
 import time as time
 
-# initialize qr code library
-qr = qrcode.QRCode(
+def shop():
+    # initialize qr code library
+    qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
     box_size=10,
     border=4,
-)
-print("=========================\n")
-# prompt user to key in the value they would like to serialize using qr  
-url = input("Enter the link you'd like to generate:\n")
-shop = input("Enter the name of the vendor/shop:\n")
-sanitize = "https://"+url
-print(sanitize)
-print("===========================\n")
-qr.add_data(sanitize)
-qr.make(fit=True)
-# You can change the color of the files
-img = qr.make_image(fill_color="#0a3d62", back_color="white")
-type(img)  # qrcode.image.pil.PilImage
-# Save the image
-img.save('images/'+shop+'.jpg')
-print("==========================\n")
-print("QR code generated\n")
-print("==========================")
+    )
+    print("=========================\n")
+    count = int(input("Enter the number of shops you'd like to generate the QR code/s for:\n"))
+    # Working with a for loop to iterate over the process based on the input above 
+    for x in range(count):
+        # prompt user to key in the value they would like to serialize using qr  
+        url = input("Enter the link you'd like to generate:\n")
+        shop = input("Enter the name of the vendor/shop:\n")
+        sanitize = "https://"+url
+        print(sanitize)
+        qr.add_data(sanitize)
+        qr.make(fit=True)
+        # You can change the color of the files
+        img = qr.make_image(fill_color="#0a3d62", back_color="white")
+        type(img)  # qrcode.image.pil.PilImage
+        # Save the image
+        img.save('images/'+shop+'.jpg')
+        print("==========================\n")
+        print("QR code generated for " + shop + "\n")
+        print("==========================")
+
+shop()
